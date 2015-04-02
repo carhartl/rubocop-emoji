b62f809e66b85f7c838e51848ea5ad63bdcf0e58
Klaus Hartl <klaus.hartl@gmail.com>
2015-04-02 19:09:14 +0200

    Adding formatter, starting with passes

---
+++ lib/rubocop/formatter/emoji_formatter.rb
@@ -0,0 +1,22 @@
+require "rubocop"
+require "rubocop/formatter/progress_formatter"
+
+module RuboCop
+  module Formatter
+    # This formatter display green hearts for files with no offenses and
+    # letters for files with problems in the them. In the end it
+    # appends the regular report data in the clang style format.
+    class EmojiFormatter < ProgressFormatter
+      def report_file_as_mark(offenses)
+        mark = if offenses.empty?
+                 "\u{1f49a}"
+               else
+                 highest_offense = offenses.max_by(&:severity)
+                 colored_severity_code(highest_offense)
+               end
+
+        output.write mark
+      end
+    end
+  end
+end
